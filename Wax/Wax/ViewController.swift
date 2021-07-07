afe66a81cf450ca09e7e8e1436ee4aebf3c57fa2
jcbauman <35509918+jcbauman@users.noreply.github.com>
2021-07-06 21:22:48 -0400

    added xcode proj

---
+++ Wax/Wax/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wax
+//
+//  Created by Jack Bauman on 7/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
